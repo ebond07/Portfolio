efef7ee893139ddfd298135c452fe06b93beda6c
Evan Bond <2130631@champlaincollege.qc.ca>
2024-02-06 01:13:45 -0500

    Update ReviewController.java

--- backend/src/main/java/com/example/backend/review/presentationLayer/ReviewController.java
+++ backend/src/main/java/com/example/backend/review/presentationLayer/ReviewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:3000","https://evanbond.me"}, allowCredentials = "true")
 @RequestMapping("reviews")
 public class ReviewController {
     private final ReviewService reviewService;
